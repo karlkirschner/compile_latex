b889207759ad0bb3627bc0acec97d28ecafed3a8
karl <karl.kirchner@h-brs.de>
2023-01-26 18:09:37 +0100

    significant upgrade to the script

--- compile_latex.py
+++ compile_latex.py
@@ -1,65 +1,82 @@
 #!/usr/bin/env python3
-"""
-This is a script that can be used to compile a latex document (e.g. filename.tex),
-    ensuring that all cross references and citations are correctly displayed. The
-    documents are compiled using pdflatex.
 
-Usage: compile_latex.py filename (i.e. minus the .tex extension)
-Requirement: pdflatex
-
-Note: the last 'remove_files()' line removes all of the Latex-generated files.
-
-Karl N. Kirschner, Ph.D.
-k.n.kirschner@gmail.com
-
-MIT License
-"""
-
+import argparse
 import os
 import subprocess
 import sys
 
 
-## remove existing Latex-generated files
-def remove_files(file_basename=None):
+def remove_files(file_basename: str):
+    ''' Remove latex files other than the TEX and PDF files.'''
+
     extensions = ['aux', 'bbl', 'blg', 'log', 'nav', 'out', 'snm',
                   'toc', 'vrb', 'dvi', 'lot', 'lof', 'glo', 'ist',
                   'bcf', 'acn', 'run.xml']
-    for ext in extensions:
-        if os.path.exists('{0}.{1}'.format(file_basename, ext)):
-            os.remove('{0}.{1}'.format(file_basename, ext))
+
+    if not isinstance(file_basename, str):
+        raise TypeError(f'file_basename (i.e. {file_basename}) was not provided as a string')
+    else:
+        for ext in extensions:
+            if os.path.exists('{0}.{1}'.format(file_basename, ext)):
+                os.remove('{0}.{1}'.format(file_basename, ext))
+
+
+def compile(file_basename: str):
+    if os.path.exists('{0}.pdf'.format(inputfile)):
+        os.remove('{0}.pdf'.format(inputfile))
+
+    ## sequence of commands
+    commands = [['pdflatex', f'{inputfile}.tex'],
+                ['bibtex',   f'{inputfile}.aux'],
+                ['pdflatex', f'{inputfile}.tex'],
+                ['pdflatex', f'{inputfile}.tex']
+               ]
+
+    for compile_tex in commands:
+       #subprocess.call(compile_tex, stdout=open(os.devnull, 'wb')) ## silent mode
+       subprocess.call(compile_tex)
 
 
 if __name__ == "__main__":
+    """ This script that compiles a latex document (e.g. filename.tex) using pdflatex,
+        ensuring that all cross references and citations are correctly displayed.
 
-    if len(sys.argv) == 2:  ## requires the command and the input filename
+        Usage: compile_latex.py -f filename (i.e. minus the .tex extension) [-d (Y/N)]
 
-        texfile = sys.argv[1]
+        Input:
+            inputfile: basename for the LaTeX (e.g. filename)
+            delete: option to delete LaTex temporary files 
 
-        if os.path.isfile('{0}.tex'.format(texfile)):
-            print(os.path.isfile('{0}.tex'.format(texfile)))
-        else:
-            print(os.path.isfile('{0}.tex'.format(texfile)))
-            raise TypeError("You did not specify a file that exists: '{0}'.".format(texfile))
+        Output:
+            files normally created when running pdflatex
+
+        Additional Requirement:
+            pdflatex
+            bibtex
+
+        Author: Karl N. Kirschner, Ph.D.
+        E-mail: karl.kirschner@h-brs.de
+
+        MIT License
+    """
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--inputfile", type=str, help="LaTeX file to compile.")
+    parser.add_argument("-d", "--delete", type=str, help="Delete temporary LaTeX files made? [Y/N]")
+    args = parser.parse_args()
+
+    inputfile = args.inputfile
+    delete = args.delete
+
+    if not os.path.isfile(f'{inputfile}.tex'):
+        raise TypeError(f'You did not specify a file, or it dose not exit that exists.')
     else:
-        raise TypeError("You did not specify a file to compile.\n\n"
-                        "Usage: compile_latex.py filename (i.e. minus the '.tex' extension)\n")
 
-    remove_files(texfile)
+        ## clean up existing files
+        remove_files(inputfile)
 
-    ## remove existing texfile.pdf
-    if os.path.exists('{0}.pdf'.format(texfile)):
-        os.remove('{0}.pdf'.format(texfile))
+        compile(inputfile)
 
-    ## sequence of commands
-    commands = [
-        ['pdflatex', '{0}.{1}'.format(texfile, 'tex')],
-        ['bibtex',   '{0}.{1}'.format(texfile, 'aux')],
-        ['pdflatex', '{0}.{1}'.format(texfile, 'tex')],
-        ['pdflatex', '{0}.{1}'.format(texfile, 'tex')]
-        ]
-
-    for compile_tex in commands:
-        subprocess.call(compile_tex)
-
-    remove_files(texfile)
+        if delete in ['Y', 'y', 'yes']:
+            print("\nRemoving LaTeX temporary files.\nDone.")
+            remove_files(file_basename=inputfile)
